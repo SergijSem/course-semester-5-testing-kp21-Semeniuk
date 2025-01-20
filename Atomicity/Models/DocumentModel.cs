fff94a977f20c6298d510b43d0c871bcab1d019d
Семенюк Сергій <semenuksergijkp@gmail.com>
2025-01-20 15:30:33 +0200

    Додано файл DocumentModel.cs

---
+++ Atomicity/Models/DocumentModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Atomicity.Models
+{
+    public class DocumentModel
+    {
+        [Required]
+        public string FileName { get; init; }
+        public long FileSize { get; set; }
+        public string FilePath { get; init; }
+
+        public DocumentModel(string fileName, string filePath) =>
+            (FileName, FilePath) = (fileName, filePath);
+    }
+}
